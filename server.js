8e4ece423b4418e0a48c6e606d2423a890eb4fc8
bobrock8 <aleksandarilic990@gmail.com>
2024-01-07 14:11:26 +0100

    feat: create first endpoint

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const dotenv = require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+
+
+app.get('/api/contacts', (req, res) => {
+    res.status(200).json({message: "Get all contacts"});
+});
+
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`);
 });
