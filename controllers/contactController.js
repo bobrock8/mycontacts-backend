fbb227d6223feff622c6b24aed7194067c478a74
bobrock8 <aleksandarilic990@gmail.com>
2024-01-08 19:40:37 +0100

    feat: wrap all methods with async handler

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,14 +1,17 @@
+// Use async handler instead of adding try/catch block to the each of the methods
+const asyncHandler = require("express-async-handler");
+
 //@desc Get all contacts
 //@route GET /api/contacts
 //@access public
-const getContacts = async (req, res) => {
+const getContacts = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Get all contacts" });
-};
+});
 
 //@desc Create a new contact
 //@route POST /api/contacts
 //@access public
-const createContact = async (req, res) => {
+const createContact = asyncHandler(async (req, res) => {
   console.log("req body", req.body);
 
   const { name, email, phone } = req.body;
@@ -18,28 +21,28 @@ const createContact = async (req, res) => {
   }
 
   res.status(201).json({ message: "Create contact" });
-};
+});
 
 //@desc Get a contact
 //@route GET /api/contacts/:id
 //@access public
-const getContactWithId = async (req, res) => {
+const getContactWithId = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Get contact with ${req.params.id}` });
-};
+});
 
 //@desc Update contact
 //@route PUT /api/contacts/:id
 //@access public
-const updateContact = async (req, res) => {
+const updateContact = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Update contact with ${req.params.id}` });
-};
+});
 
 //@desc Delete contact
 //@route DELETE /api/contacts/:id
 //@access public
-const deleteContact = async (req, res) => {
+const deleteContact = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete contact with ${req.params.id}` });
-};
+});
 
 module.exports = {
   getContacts,
